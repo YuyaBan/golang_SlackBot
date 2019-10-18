41ef3b0446328b045762622ce1e09898bbc9df06
YuyaBan <shepherd.1021@gmail.com>
2019-10-18 21:10:40 +0900

    fix

--- main.go
+++ main.go
@@ -8,27 +8,16 @@ import (
 	"github.com/nlopes/slack"
 )
 
-type Env struct {
-	Config Config
-	rtm    *slack.RTM
-}
-
-type Config struct {
-	Token string
-	Botid string
-}
-
 func main() {
-    var env Env
-    env.Config := domain.NewEnviroment()
+	env := domain.NewEnviroment()
 
-	env.rtm := slack.New(env.Config.Token).NewRTM()
+	rtm := slack.New(env.Token).NewRTM()
 	go rtm.ManageConnection()
 
 	for msg := range rtm.IncomingEvents {
 		switch ev := msg.Data.(type) {
 		case *slack.MessageEvent:
-			if err := env.ValidateMessageEvent(ev, rtm); err != nil {
+			if err := ValidateMessageEvent(ev, rtm, env); err != nil {
 				log.Printf("[ERROR] Failed to handle message: %s", err)
 			}
 		}
@@ -36,16 +25,22 @@ func main() {
 }
 
 // ValidateMessageEvent send Message
-func (env *Env) ValidateMessageEvent(ev *slack.MessageEvent, rtm *slack.RTM) error {
-	// Only response in specific channel. Ignore else.
+func ValidateMessageEvent(ev *slack.MessageEvent, rtm *slack.RTM, env *domain.Config) error {
 
 	// Only response mention to bot. Ignore else.
 	log.Print(ev.Msg.Text)
-	if !strings.HasPrefix(ev.Msg.Text, config.API.Botid) {
+	if !strings.HasPrefix(ev.Msg.Text, env.Botid) {
 		//log.Printf("%s %s", ev.Channel, ev.Msg.Text)
 		return nil
 	}
 
-	rtm.SendMessage(rtm.NewOutgoingMessage("メンション付いてるな。:imouto: くれるのか？", ev.Channel))
+	if strings.Contains(ev.Msg.Text, "imouto") {
+		rtm.SendMessage(rtm.NewOutgoingMessage(":imouto: が欲しいのか？ わかるぞ。", ev.Channel))
+	} else if strings.Contains(ev.Msg.Text, "nahemohu") {
+		rtm.SendMessage(rtm.NewOutgoingMessage(":nahemohu: がそんなに好き？", ev.Channel))
+	} else {
+		rtm.SendMessage(rtm.NewOutgoingMessage("呼んだ？。どうした？", ev.Channel))
+	}
+
 	return nil
 }
