ec9ca46957d3b2d4a636243b3a5ca7349b490489
YuyaBan <shepherd.1021@gmail.com>
2019-10-18 20:52:38 +0900

    fix

--- domain/config.go
+++ domain/config.go
@@ -2,14 +2,9 @@ package domain
 
 import (
 	"github.com/BurntSushi/toml"
-	"github.com/nlopes/slack"
 )
 
 // Env should have config and rtm
-type Env struct {
-	Config Config
-	rtm    *slack.RTM
-}
 
 // Config should have Slack Config
 type Config struct {
@@ -18,19 +13,14 @@ type Config struct {
 }
 
 // NewEnviroment return Config and rtm
-func NewEnviroment(configfile string) *Env {
+func NewEnviroment() *Config {
 
 	var config Config
 
-	_, err := toml.DecodeFile(configfile, &config)
+	_, err := toml.DecodeFile("config.toml", &config)
 	if err != nil {
 		// Error Handling
 	}
 
-	slackrtm := slack.New(config.Token).NewRTM()
-
-	return &Env{
-		Config: config,
-		rtm:    slackrtm,
-	}
+	return &config
 }
